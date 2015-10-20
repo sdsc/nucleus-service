96c53d2bb55d916fe2304b011e61344ac25e2722
Dmitry Mishin <dmishin@sdsc.edu>
2015-10-19 17:41:16 -0700

    Autoreload app

--- nucleus_service/nucleus/wsgi.py
+++ nucleus_service/nucleus/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nucleus.settings")
 
 application = get_wsgi_application()
+
+import nucleus.monitor
+nucleus.monitor.start(interval=1.0)
