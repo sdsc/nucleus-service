f00aa8a282c3e76df6e8e002140eb9a09be3cad4
Dmitry Mishin <dmitry.mishin@gmail.com>
2016-08-11 15:40:49 -0700

    CMT-698 Store nucleus secret in a separate file

--- nucleus_service/nucleus/settings.py
+++ nucleus_service/nucleus/settings.py
@@ -21,7 +21,8 @@ HOME_DIR = os.getenv("HOME")
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qy4-11q&&19jaz!dwn7mk6r#zh+=iiprtzxn^@698$t*e(+f%#'
+with open('/var/secrets/cometvc/django_secret') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
